efa591402419c4921d4915b992b29efe4384b80c
Adam Jankowski <ghost17814@gmail.com>
2023-06-01 10:15:26 -0400

    yeeyee

--- Bored/BoredRepository.cs
+++ Bored/BoredRepository.cs
@@ -13,8 +13,12 @@ namespace Bored
             var url = "http://www.boredapi.com/api/activity/";           
             var randomToDo = client.GetStringAsync(url).Result;
             doThis.Activity = JObject.Parse(randomToDo).GetValue("activity").ToString();
+            doThis.Link = JObject.Parse(randomToDo).GetValue("link").ToString();
             return doThis;
             //if null differrent link 
         }
+
+
+       
     }
 }
